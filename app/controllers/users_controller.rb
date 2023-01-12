dfdc98246e7c974f3fbd941a68eb4e5b2263a409
abraham <apolichenco@gmail.com>
2023-01-12 14:06:24 -0500

    commit tree f88d414cb2efd5537d1bfd770252db994cfb3795

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create(params.permit(:username, :password, :password_confirmation))
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else 
+            render json: { error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+end
